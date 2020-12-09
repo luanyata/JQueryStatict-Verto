9c646a4412a3d61f863ebfd97e0af9871bff0d2f
Luan Lima <luan@yata.dev>
2020-12-09 09:59:20 -0300

    Novas tipagens
    Adição e correções das tipagens

--- JQueryStatic.d.ts
+++ JQueryStatic.d.ts
@@ -1,60 +1,107 @@
 interface JQueryStatic {
-  verto: Verto
-  FSRTC: { checkPerms(callback: Function, audio: boolean, video: boolean): void }
+	verto: Verto
+	FSRTC: { checkPerms(callback: Function, audio: boolean, video: boolean): void }
 }
 
+type StateCall =
+	'answering' | 'active' | 'destroy' | 'early' | 'hangup' | 'held' | 'inbound' | 'recovering' | 'ringing' | 'trying'
+
 interface Verto {
-  new(...params: any): Agent;
-  init(...params: any): any
-  refreshDevices(callback: Function): any
-  genUUID(): string
-  audioInDevices: [],
-  audioOutDevices: [],
-  videoInDevices: [],
-  videoOutDevices: []
+	new(...params: any): Agent;
+	init(...params: any): any
+	refreshDevices(callback: Function): any
+	genUUID(): string
+	audioInDevices: [],
+	audioOutDevices: [],
+	videoInDevices: [],
+	videoOutDevices: []
 }
 
 interface AgentData {
-  login: string,
-  passwd: string
+	login: string,
+	passwd: string
 }
 
+interface VideoParams {
+	minWidth: number,
+	minHeight: number,
+	maxWidth: number,
+	maxHeight: number,
+	minFrameRate: number,
+	vertoBestFrameRate: string
+}
+
+interface CallParams {
+	outgoingBandwidth: string,
+	incomingBandwidth: string,
+	useStereo: boolean,
+	useVideo: boolean,
+	useCamera: boolean,
+	useSpeak: string,
+	screenShare: boolean,
+	dedEnc: boolean,
+	mirrorInput: boolean
+}
+
+interface NewCallParams extends CallParams {
+	destination_number: string,
+	caller_id_name: string,
+	caller_id_number: string,
+}
+
+type AnswerCallParams = CallParams;
+
 interface Agent {
-  logout(): void;
-  login(): void
-  loginData(credential: AgentData): void
-  newCall(params: any): void
+	broadcast(channel: any, params: any): void
+	deviceParams(obj: any): void
+	handleMessage(params: any): void
+	hangup(callId?: string): void
+	iceServers(obj: any): void
+	login(): void
+	loginData(credential: AgentData): void
+	logout(): void;
+	message(msg: any): boolean
+	newCall(params: NewCallParams): Dialog
+	processReply(method: string, sucess: boolean, e: any): void
+	purge(): void
+	sendMethod(method: any, obj: any): void
+	subscriber(channel: any, sparams: any): []
+	unsubscribe(handle: any): void
+	videoParams(params: VideoParams): void
 }
 
 interface Dialog {
-  invite(): void
-  sendMethod(method: any, obj: any): void
-  setAudioPlaybackDevice(sinkId: any, callback: any, arg: any): void
-  setState(state: string): void
-  processReply(method: string, success: string, e: string): void
-  hangup(params: any): void
-  stopRinging(): void
-  indicateRing(): void
-  ring(): void
-  useVideo(on: any): void
-  setMute(what: any): void
-  getMute(): void
-  setVideoMute(what: any): void
-  getVideoMute(): void
-  useStereo(on: any): void
-  dtmf(digit: string): void
-  rtt(obj: any): void
-  transfer(dest: string, params: any): void
-  replace(replaceCallID: any, params: any): void
-  hold(params: any): void
-  unhold(params: any): void
-  toggleHold(params: any): void
-  message(msg: string): boolean
-  answer(params: any): void
-  handleAnswer(params: any): void
-  cidString(enc: any): void
-  sendMessage(msg: string, params: any): void
-  handleInfo(params: any): void
-  handleDisplay(params: any): void
-  handleMedia(params: any): void
+	direction: { name: StateCall }
+	callID: string;
+	state: { name: string }
+	answer(params: AnswerCallParams): void
+	cidString(enc: any): void
+	dtmf(digit: string): void
+	getMute(): void
+	getVideoMute(): void
+	hangup(params?: any): void
+	handleAnswer(params: any): void
+	handleDisplay(params: any): void
+	handleInfo(params: any): void
+	handleMedia(params: any): void
+	hold(params?: any): void
+	indicateRing(): void
+	invite(): void
+	message(msg: string): boolean
+	processReply(method: string, success: string, e: string): void
+	replace(replaceCallID: any, params: any): void
+	ring(): void
+	rtt(obj: any): void
+	sendMessage(msg: string, params: any): void
+	sendMethod(method: any, obj: any): void
+	setAudioPlaybackDevice(sinkId: any, callback: any, arg: any): void
+	setMute(what: any): void
+	setState(state: string): void
+	setVideoMute(what: any): void
+	stopRinging(): void
+	transfer(dest: string, params: any): void
+	toggleHold(params: any): void
+	unhold(params?: any): void
+	useStereo(on: any): void
+	useVideo(on: any): void
 }
